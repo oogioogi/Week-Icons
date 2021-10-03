81d519ca1ee308113ef0dfa35e14c4f9a35c9afd
oogioogi <detacl73@gmail.com>
2021-10-03 10:23:54 +0900

    Initial Commit

---
+++ Analytics/AnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsApp.swift
+//  Analytics
+//
+//  Created by 이용석 on 2021/10/03.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Analytics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Analytics
+//
+//  Created by 이용석 on 2021/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
